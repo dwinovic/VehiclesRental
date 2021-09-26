9884342d281bfbcd67c9e9655bd154eac689c7c5
dwinovic <nd.cahya19@gmail.com>
2021-09-26 18:15:41 +0700

    update env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    // Will be available on both server and client
-    backendUrl: process.env.NEXT_PUBLIC_SERVER,
     frontEndUrl: process.env.NEXT_PUBLIC_URL_FRONTEND,
     imagesServer: process.env.NEXT_PUBLIC_IMAGES,
   },
